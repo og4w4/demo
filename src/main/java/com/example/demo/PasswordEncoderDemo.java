5977c8573b709fc36049007d53e5c9b56c9ab7a5
tester <sample@sample.com>
2024-08-20 22:44:36 +0900

    f

--- src/main/java/com/example/demo/PasswordEncoderDemo.java
+++ src/main/java/com/example/demo/PasswordEncoderDemo.java
@@ -5,7 +5,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class PasswordEncoderDemo {
     public static void main(String[] args) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        String rawPassword = "002"; // 元のパスワード
+        String rawPassword = "password"; // 元のパスワード
         String encodedPassword = encoder.encode(rawPassword);
         System.out.println("Encoded password: " + encodedPassword);
     }
